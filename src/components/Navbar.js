2ce29d80684247c45be3b7b3b7fcf83d37bf17ea
Hoang Liem Nguyen <daniel.liemng@gmail.com>
2020-01-12 23:09:25 -0500

    Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,33 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        <h2 className="display-">Stay Connected</h2>
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <h2 className="display-">Stay Connected</h2>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
-              Home{" "}
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home{" "}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
